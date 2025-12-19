9fec0f7f2d22638f64ec1843dd4d3c0588382e9b
Muhammad Ahmad <ahmadkhan20093@gmail.com>
2025-12-19 15:19:28 +0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "currency-converter-v1";
+const urlsToCache = [
+  "index.html",
+  "manifest.json",
+  "icon-192.png",
+  "icon-512.png"
+];
+
+// Install SW
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch (offline support)
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+
